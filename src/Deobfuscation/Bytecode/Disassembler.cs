35bfa70c617a47bf98cff65948c1be8364170268
Z8W4K9P7M5VF2A6RI1L0DE3NT <kamsikamsi116@gmail.com>
2025-12-19 04:21:31 -0500

    Update Disassembler.cs

--- src/Deobfuscation/Bytecode/Disassembler.cs
+++ src/Deobfuscation/Bytecode/Disassembler.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
 using MoonsecDeobfuscator.Bytecode.Models;
 using Function = MoonsecDeobfuscator.Bytecode.Models.Function;
 
@@ -22,71 +21,69 @@ public class Disassembler(Function rootFunction)
 
     public string Disassemble()
     {
-        var ast = BuildFunctionNode(rootFunction, "MainScript", false);
-        _builder.AppendLine("-- Reconstructed High-Level Source");
-        _builder.AppendLine("-- Fixed VM Call Shadowing & Variable Redundancy\n");
-        PrintFunctionNode(ast);
-        _builder.AppendLine("\nMainScript()");
+        _builder.AppendLine("-- Decompiled with High-Level Flow Recovery");
+        var main = BuildFunctionNode(rootFunction, "Main", false);
+        PrintBlock(main.Body);
         return _builder.ToString();
     }
 
     private FunctionNode BuildFunctionNode(Function function, string name, bool isAnon)
     {
         var statements = new List<AstNode>();
-        var registerState = new Dictionary<int, string>();
-        var declaredInScope = new HashSet<string>();
+        var regs = new Dictionary<int, string>();
+        var insts = function.Instructions;
 
-        string Get(int r) => registerState.TryGetValue(r, out var val) ? val : $"v{r}";
-
-        for (int i = 0; i < function.Instructions.Count; i++)
+        for (int i = 0; i < insts.Count; i++)
         {
-            var ins = function.Instructions[i];
+            var ins = insts[i];
+
+            // 1. Check for Loops (Jumping backwards)
+            if (ins.OpCode == OpCode.Jmp && ins.B < 0)
+            {
+                statements.Add(new RawNode("end -- loop end"));
+                continue;
+            }
 
             switch (ins.OpCode)
             {
                 case OpCode.GetGlobal:
-                    string gName = ((StringConstant)function.Constants[ins.B]).Value;
-                    registerState[ins.A] = gName;
-                    AddLocal(statements, gName, gName, declaredInScope);
+                    regs[ins.A] = ((StringConstant)function.Constants[ins.B]).Value;
                     break;
 
                 case OpCode.LoadK:
-                    string k = FormatConst(function, ins.B);
-                    registerState[ins.A] = k;
-                    // Don't write every constant to a variable unless it's a long string/URL
-                    if (k.Length > 15) AddLocal(statements, $"c{ins.A}", k, declaredInScope);
-                    break;
-
-                case OpCode.Call:
-                    int argCount = Math.Max(0, ins.B - 1);
-                    var args = Enumerable.Range(ins.A + 1, argCount).Select(Get).ToList();
-                    string caller = Get(ins.A);
-
-                    // --- VM RESOLVER ---
-                    // Detect if calling 'game' to get a service
-                    if (caller == "game" && args.Count > 0) {
-                        string service = args[0].Replace("\"", "");
-                        registerState[ins.A] = service;
-                        AddLocal(statements, service, $"game:GetService({args[0]})", declaredInScope);
-                    } else {
-                        statements.Add(new CallNode(caller, args));
-                    }
+                    regs[ins.A] = FormatConst(function, ins.B);
                     break;
 
                 case OpCode.GetTable:
-                    string tbl = Get(ins.B);
+                    string tbl = GetReg(regs, ins.B);
                     string key = RK(ins.C, function).Replace("\"", "");
-                    string fullName = (tbl == "game" || tbl == "workspace") ? key : $"{tbl}.{key}";
+                    regs[ins.A] = (tbl == "game") ? $"game:GetService(\"{key}\")" : $"{tbl}.{key}";
+                    break;
+
+                case OpCode.Call:
+                    string fName = GetReg(regs, ins.A);
+                    var args = Enumerable.Range(ins.A + 1, Math.Max(0, ins.B - 1))
+                                         .Select(r => GetReg(regs, r)).ToList();
                     
-                    registerState[ins.A] = key;
-                    AddLocal(statements, key, fullName, declaredInScope);
+                    // Detect if this call is actually an event connection or UI method
+                    if (args.Any(a => a.Contains("function")))
+                        statements.Add(new CallNode($"{fName}:Connect", args));
+                    else
+                        statements.Add(new CallNode(fName, args));
                     break;
 
                 case OpCode.Closure:
-                    var child = BuildFunctionNode(function.Functions[ins.B], "", true);
-                    registerState[ins.A] = "func_" + ins.B;
-                    statements.Add(new AssignNode(registerState[ins.A], "function()", true));
-                    statements.Add(child);
+                    var childFunc = function.Functions[ins.B];
+                    var childNode = BuildFunctionNode(childFunc, "", true);
+                    regs[ins.A] = FormatAnonymousFunc(childNode);
+                    break;
+
+                case OpCode.Eq:
+                case OpCode.Lt:
+                case OpCode.Le:
+                    // Pattern for "if condition then"
+                    string cond = $"{RK(ins.B, function)} {(ins.OpCode == OpCode.Eq ? "==" : "<=")} {RK(ins.C, function)}";
+                    statements.Add(new RawNode($"if {(ins.A == 0 ? cond : "not " + cond)} then"));
                     break;
             }
         }
@@ -94,33 +91,36 @@ public class Disassembler(Function rootFunction)
         return new FunctionNode(name, new Block(statements), isAnon);
     }
 
-    private void AddLocal(List<AstNode> stats, string name, string val, HashSet<string> declared)
+    private string FormatAnonymousFunc(FunctionNode node)
     {
-        // Prevent shadowing and redundant re-definitions
-        if (name.StartsWith("\"") || char.IsDigit(name[0])) return;
-        bool isNew = declared.Add(name);
-        stats.Add(new AssignNode(name, val, isNew));
+        var sb = new StringBuilder();
+        sb.AppendLine("function(...)");
+        foreach (var stat in node.Body.Statements)
+        {
+            if (stat is CallNode c) sb.AppendLine($"    {c.Func}({string.Join(", ", c.Args)})");
+            if (stat is RawNode r) sb.AppendLine($"    {r.Code}");
+        }
+        sb.Append("end");
+        return sb.ToString();
     }
 
-    private void PrintFunctionNode(FunctionNode fn)
+    private void PrintBlock(Block block)
     {
-        string indent = new string(' ', _indent * 4);
-        if (!fn.IsAnonymous) _builder.AppendLine($"{indent}local function {fn.Name}()");
-        _indent++;
-        foreach (var node in fn.Body.Statements) PrintAstNode(node);
-        _indent--;
-        if (!fn.IsAnonymous) _builder.AppendLine($"{indent}end");
-        else _builder.AppendLine($"{indent}end");
+        string indentStr = new string(' ', _indent * 4);
+        foreach (var node in block.Statements)
+        {
+            if (node is CallNode c) 
+                _builder.AppendLine($"{indentStr}{c.Func}({string.Join(", ", c.Args)})");
+            else if (node is RawNode r)
+            {
+                if (r.Code.StartsWith("end")) _indent--;
+                _builder.AppendLine($"{new string(' ', _indent * 4)}{r.Code}");
+                if (r.Code.EndsWith("then") || r.Code.Contains("do")) _indent++;
+            }
+        }
     }
 
-    private void PrintAstNode(AstNode node)
-    {
-        string indent = new string(' ', _indent * 4);
-        if (node is AssignNode a) _builder.AppendLine($"{indent}{(a.IsLocal ? "local " : "")}{a.Left} = {a.Right}");
-        else if (node is CallNode c) _builder.AppendLine($"{indent}{c.Func}({string.Join(", ", c.Args)})");
-        else if (node is FunctionNode f) PrintFunctionNode(f);
-    }
-
+    private string GetReg(Dictionary<int, string> regs, int r) => regs.TryGetValue(r, out var v) ? v : $"v{r}";
     private string RK(int val, Function f) => val >= 256 ? FormatConst(f, val - 256) : $"v{val}";
     private string FormatConst(Function f, int i) => f.Constants[i] is StringConstant s ? $"\"{s.Value}\"" : f.Constants[i].ToString();
 }
